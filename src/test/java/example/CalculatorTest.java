243dd806e8428c094b8f95affcf6a417ec1293a3
r.legrand <remi.legrand@le-campus-numerique.fr>
2024-08-28 11:22:10 +0200

    Added test assertion

--- src/test/java/example/CalculatorTest.java
+++ src/test/java/example/CalculatorTest.java
@@ -1,37 +1,31 @@
 package example;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.example.Calculator;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 public class CalculatorTest {
     @Test
     public void testAdd() {
         Calculator calc = new Calculator();
-        calc.add(1, 2);
-        calc.add(1, -3);
-        calc.subtract(1, 0);
+        assertEquals(3, calc.add(1,2));
     }
 
     @Test
     public void testSubtract() {
         Calculator calc = new Calculator();
-        calc.subtract(1, 2);
-        calc.subtract(1, 0);
-        calc.subtract(1, -3);
+        assertEquals(-1, calc.subtract(1, 2));
     }
 
     @Test
     public void testMultiply() {
         Calculator calc = new Calculator();
-        calc.multiply(1, 2);
-        calc.multiply(1, -3);
-        calc.subtract(1, 0);
+        assertEquals(2, calc.multiply(1, 2));
     }
 
     @Test
     public void testDivide() {
         Calculator calc = new Calculator();
-        calc.divide(1, 2);
-        calc.divide(1, -10);
+        assertEquals(1, calc.divide(2, 2));
     }
 }
